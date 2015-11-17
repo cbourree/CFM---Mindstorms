025d4a21a5abe14bd41f354ae89b182adeb94e8f
Charles B <cbourree@hotmail.fr>
2015-11-17 08:54:42 +0000

    test

--- Python/test.py
+++ Python/test.py
@@ -11,7 +11,7 @@ print("Initialisation du moteur A...")
 A = Moteur('A', -80)
 B = Moteur('B', -80)
 C = Moteur('C', -80)
-#A.runMS(15000, 50)
+A.runMS(15000, 50)
 print("Moteur A" + str(A.isRuning()) + "Moteur B" + str(B.isRuning()) + "Moteur C" + str(C.isRuning()))
 
 print("Initialisation du moteur B...")
@@ -20,9 +20,8 @@ print("Moteur A" + str(A.isRuning()) + "Moteur B" + str(B.isRuning()) + "Moteur 
 
 print("Initialisation du moteur C...")
 #C.setConsigne(-50)
-#C.runMS(9000, 50)
+C.runMS(9000, 50)
 print("Moteur A" + str(A.isRuning()) + "Moteur B" + str(B.isRuning()) + "Moteur C" + str(C.isRuning()))
-
 time.sleep(5)
 print("5 secondes plus tard...")
 print("Moteur A" + str(A.isRuning()) + "Moteur B" + str(B.isRuning()) + "Moteur C" + str(C.isRuning()))
