c8ba47157b1f6475be969ed7bac959e0e672534e
Charles B <cbourree@hotmail.fr>
2015-11-24 10:36:08 +0000

    TouchCaptor

--- Python/TouchCapteurs.py
+++ Python/TouchCapteurs.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
 
-import CAN
+from CAN import getTension
 import RPi.GPIO as GPIO
 
 class CapteurPortErreur(Exception):
@@ -12,21 +12,18 @@ class TouchCapteur():
 
     _PORTS = "1234" #Liste des ports disponibles
     
-    def __new__(self, port):
+    def __init__(self, port):
         if port not in TouchCapteur._PORTS:
             raise CapteurPortErreur
         self._port = port
         if port == '1':
-            self._entree = 0
+            self._entree = 4
         elif port == '2':
             self._entree = 1
-        elif port == '3'
+        elif port == '3':
             self._entree = 2
         else:
             self._entree = 3
-        GPIO.setup(pinA, GPIO.IN)
-        GPIO.setup(pinB, GPIO.IN)
-        return self
 
     def getPort(self):
         return self._port
@@ -37,7 +34,7 @@ class TouchCapteur():
         self._port = port
 
     def isPressed(self):
-        tension = CAN.getTension(self._entree)
+        tension = getTension(self._entree)
         if tension < 2:
             return False
         else:
--- Python/test_capteurs.py
+++ Python/test_capteurs.py
@@ -3,11 +3,13 @@
 
 import RPi.GPIO as GPIO
 from TouchCapteurs import TouchCapteur
+import time
 
 Capteur1 = TouchCapteur('1')
 
 while 1:
-    if (Capteur1.isPressed()):
-        print("Le capteur est appuez")
+    if (Capteur1.isPressed() == 1):
+        print("Le capteur est appuyez")
     else:
         print("Le capteur est relaché")
+    time.sleep(1)
